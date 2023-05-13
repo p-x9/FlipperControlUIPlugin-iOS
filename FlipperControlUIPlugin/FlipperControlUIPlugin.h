1264bb42bbc6fa7ca3fb010948396b0a9affc7de
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-05-14 01:01:19 +0900

    Initial Commit

---
+++ FlipperControlUIPlugin/FlipperControlUIPlugin.h
@@ -0,0 +1,19 @@
+//
+//  FlipperControlUIPlugin.h
+//  FlipperControlUIPlugin
+//
+//  Created by p-x9 on 2023/05/14.
+//  
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlipperControlUIPlugin.
+FOUNDATION_EXPORT double FlipperControlUIPluginVersionNumber;
+
+//! Project version string for FlipperControlUIPlugin.
+FOUNDATION_EXPORT const unsigned char FlipperControlUIPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlipperControlUIPlugin/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'FlipperControlUIPlugin' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for FlipperControlUIPlugin
+  pod 'FlipperKit'
+
+  target 'FlipperControlUIPluginTests' do
+    # Pods for testing
+  end
+
+end
